10e7913e087a1f94fa7bc015c97f94577036febf
blurdel <5648737+blurdel@users.noreply.github.com>
2025-02-12 20:03:10 -0700

    Added the weather request function

---
+++ src/main/java/com/blurdel/springai/functions/WeatherServiceFunction.java
@@ -0,0 +1,44 @@
+package com.blurdel.springai.functions;
+
+import com.blurdel.springai.model.WeatherRequest;
+import com.blurdel.springai.model.WeatherResponse;
+import org.springframework.web.client.RestClient;
+
+import java.util.function.Function;
+
+public class WeatherServiceFunction implements Function<WeatherRequest, WeatherResponse> {
+
+    public static final String WEATHER_URL = "https://api.api-ninjas.com/v1/weather";
+
+    private final String apiNinjaKey;
+
+    public WeatherServiceFunction(String apiNinjaKey) {
+        this.apiNinjaKey = apiNinjaKey;
+    }
+
+    @Override
+    public WeatherResponse apply(WeatherRequest weatherRequest) {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(WEATHER_URL)
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.set("X-Api-Key", apiNinjaKey);
+                    httpHeaders.set("Accept", "application/json");
+                    httpHeaders.set("Content-Type", "application/json");
+                }).build();
+        
+        return restClient.get().uri(uriBuilder -> {
+            System.out.println("Building URI for weather request: " + weatherRequest);
+
+            uriBuilder.queryParam("city", weatherRequest.location());
+
+            if (weatherRequest.state() != null && !weatherRequest.state().isBlank()) {
+                uriBuilder.queryParam("state", weatherRequest.state());
+            }
+            if (weatherRequest.country() != null && !weatherRequest.country().isBlank()) {
+                uriBuilder.queryParam("country", weatherRequest.country());
+            }
+            return uriBuilder.build();
+        }).retrieve().body(WeatherResponse.class);
+    }
+
+}
--- src/main/java/com/blurdel/springai/services/OpenAIServiceImpl.java
+++ src/main/java/com/blurdel/springai/services/OpenAIServiceImpl.java
@@ -13,35 +13,9 @@ public class OpenAIServiceImpl implements OpenAIService {
 
     final ChatModel chatModel;
 
-//    @Value("classpath:/templates/rag-prompt-template.st")
-//    private Resource ragPromptTemplate;
-
-//    @Value("classpath:/templates/system-message.st")
-//    private Resource systemMessageTemplate;
-
     @Override
     public Answer getAnswer(Question question) {
         return new Answer("to-do implement me!");
     }
-
-//    @Override
-//    public Answer getAnswer(Question question) {
-//        PromptTemplate sysMsgPT = new SystemPromptTemplate(systemMessageTemplate);
-//        Message sysMsg = sysMsgPT.createMessage();
-//
-//        List<Document> documents = vectorStore.similaritySearch(
-//                SearchRequest.builder().query(question.question()).topK(6).build());
-//        List<String> contentList = documents.stream().map(Document::getContent).toList();
-//
-//        PromptTemplate pt = new PromptTemplate(ragPromptTemplate);
-//        Message userMsg = pt.createMessage(Map.of(
-//                "input", question.question(),
-//                "documents", String.join("\n", contentList)
-//            ));
-//
-//        ChatResponse resp = chatModel.call(new Prompt(List.of(sysMsg, userMsg)));
-//
-//        return new Answer(resp.getResult().getOutput().getContent());
-//    }
 
 }
